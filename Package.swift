1566873914733879792620e5efcbc80220916e14
woxtu <woxtup@gmail.com>
2023-04-30 17:51:48 +0900

    Fix the library name

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SSSpinnerButtonr",
+            name: "SSSpinnerButton",
             targets: ["SSSpinnerButton"]),
     ],
     dependencies: [
