a684f128f3a0fbbbb5c4421f2d832ecccb898a4c
Purva Rupareliya <purva.r@simformsolutions.com>
2023-12-22 16:09:55 +0530

    PR for testing hound

--- SSSpinnerButton/SpinerShapes/SpinnerShapeWithAnimation/SSCircleScaleMultiple.swift
+++ SSSpinnerButton/SpinerShapes/SpinnerShapeWithAnimation/SSCircleScaleMultiple.swift
@@ -59,4 +59,45 @@ class SSCircleScaleMultiple: SSSpinnerAnimationDelegate {
             layer.addSublayer(circle)
         }
     }
+
+    func setupSpinnerAnimation(layer: CALayer, frame: CGRect, color: UIColor, spinnerSize: UInt?) {
+        let sizeValue =min(frame.width, frame.height)
+        let center =CGPoint(x: 0, y: 0)
+        let duration:CFTimeInterval = 1
+        let beginTime = CACurrentMediaTime()
+        let beginTimes = [0, 0.2, 0.4]
+
+        // Scale animation
+        let scaleAnimation =CABasicAnimation(keyPath: "transform.scale")
+
+        scaleAnimation.duration = duration
+        scaleAnimation.fromValue = 0
+        scaleAnimation.toValue = 1
+
+        // Opacity animation
+        let opacityAnimation = CAKeyframeAnimation(keyPath: "opacity")
+        opacityAnimation.duration = duration
+        opacityAnimation.keyTimes = [0, 0.05, 1]
+        opacityAnimation.values = [0, 1, 0]
+
+        // Animation
+        let animation = CAAnimationGroup()
+
+        animation.animations = [scaleAnimation, opacityAnimation]
+        animation.timingFunction = CAMediaTimingFunction(name: .linear)
+        animation.duration = duration
+        animation.repeatCount = HUGE
+        animation.isRemovedOnCompletion = false
+
+        // Draw balls
+        for i in 0 ..< 3 {
+            let circle = SpinnerShape.circle.layerWith(size: CGSize(width: sizeValue, height: sizeValue), color: color)
+            let frame = CGRect(x: center.x, y: center.y, width: sizeValue, height: sizeValue)
+            animation.beginTime = beginTime + beginTimes[i]
+            circle.frame = frame
+            circle.opacity = 0
+            circle.add(animation, forKey: "animation")
+            layer.addSublayer(circle)
+        }
+    }
 }
